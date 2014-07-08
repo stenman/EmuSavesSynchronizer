bf4472512c7d7a39c739a7d4a471ecb17e0c49ea
stenman <roger.stenman@gmail.com>
2014-07-09 01:34:05 +0200

    -Added basics for logging and testing etc -Created some simple test code

--- src/main/java/com/example/filecopyservice/FileCopyService/App.java
+++ src/main/java/com/example/filecopyservice/FileCopyService/App.java
@@ -1,13 +1,21 @@
 package com.example.filecopyservice.FileCopyService;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		while (true) {
+
+			try {
+				logger.info("It's alive!");
+				Thread.sleep(2000);
+			} catch (java.lang.InterruptedException ex) {
+				return;
+			}
+		}
+	}
 }
