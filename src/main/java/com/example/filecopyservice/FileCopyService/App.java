7f2c89024f5a75e610ef45c9f119fefc80d3f77e
stenman <roger.stenman@gmail.com>
2014-07-08 23:34:40 +0200

    -First commit

---
+++ src/main/java/com/example/filecopyservice/FileCopyService/App.java
@@ -0,0 +1,13 @@
+package com.example.filecopyservice.FileCopyService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
